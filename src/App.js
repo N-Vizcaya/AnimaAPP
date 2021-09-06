0a1daa99b32e412bee542fb43909162722eb3928
N-Vizcaya <83844148+N-Vizcaya@users.noreply.github.com>
2021-09-06 18:51:54 -0300

    agregando contador

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import './App.css';
 import NavBar from './components/NavBar.js'
+import { useState } from 'react';
 import marca from './components/animalogo.png'
 import Main from './components/Main.js'
-import data from './components/data.js'
+import Contador from './components/ItemCont.js'
+
+
 
 
 
  const App = () => {
+
+
+
  return (
 
- <div className="App">
-    <header>
-        <NavBar/>
-   <div className="row">
-        <Main/>
-        
-   </div>    
-    </header>
+  <div className="App">
+<header>
+    <NavBar/>
+     
+</header>
+
+<body>
+<div className="row">     
+<Main/>
+</div>  
+
+<Contador/>
+    
+    
+
+
+</body>
+
  </div>
 
     );  
 
- }
+}
 
 
 
---
+++ src/components/ItemCont.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const Contador = () => {
+const [count, setCount] = useState(0);
+const miFuncionRestar = () => {
+    if (count !== 0){
+    setCount(count -1)
+
+    }else{
+        alert("El carrito esta vacio") //el alert es solo de prueba, no va a estar en el proyecto final
+    }
+    }
+    
+
+ 
+    return (
+
+<div> 
+    <h1>{count}</h1>
+    <button onClick={() => setCount(count + 1)}> Sumar </button>
+    <button onClick={miFuncionRestar} > Restar </button>
+
+</div>
+ )
+ 
+} 
+
+
+
+export default Contador
