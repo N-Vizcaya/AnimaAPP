934ef185b0fb19fec723fab329149d8bebd08fd2
N-Vizcaya <83844148+N-Vizcaya@users.noreply.github.com>
2021-09-11 13:10:22 -0300

    update map

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,6 +5,24 @@ import css from './Card.css'
 import Contador from './ItemCont.js'
 
 
+export default class Item extends React.Component{
+  render(){
+      return (
+          <div className="card">
+              <div className="card-logo">
+                  <img src={pesas} width='300' alt="imgCard" />
+              </div>
+              <div className="card-content">
+                <h1>Funcional</h1>  
+                <p>Entrenamientos funcionales diarios</p>
+                <p> $2500 </p>
+              </div>
+          </div>
+      )
+  }
+}
+
+
 
 
 
