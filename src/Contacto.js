0f9985fa3bf4026d822f07171aa57c17f071e815
N-Vizcaya <83844148+N-Vizcaya@users.noreply.github.com>
2021-09-20 16:40:32 -0300

    Router II

--- src/Contacto.js
+++ src/Contacto.js
@@ -11,11 +11,13 @@ const Contacto = () => {
     return(
 <div>
         <h1> Contactenos </h1> 
-        
-        </div>
+        <label><input name="name" className="name" placeholder="Nombre" type="text" /></label>
+        <label><input name="email" className="email" placeholder="Email" type="text" /></label>
+        <label><input name="consulta" className="text" placeholder="Escriba su consulta" type="textarea" /></label>
+        <label><input type="submit" className="btn btn-submit" value="Submit" /></label>
+</div>
+
     )
-
-
 }
 
 export default Contacto
